55521df5ecac6b4d35e1f87057bf6d16c37de32e
Razyar Saeedian <razyarsaeedian@gmail.com>
2020-06-15 20:45:36 +0430

    Add files via upload

---
+++ face_recognition.py
@@ -0,0 +1,37 @@
+'''
+github.com/razyar
+khoderazyar.ir
+'''
+
+import cv2
+import sys
+
+
+
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print("i found {0} faces in this pic".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Github.com/razyar", image)
+cv2.waitKey(0)
